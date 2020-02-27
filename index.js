deaa14265b510371afcbe1d8c986bdbc5da0d69d
Suthura Sudharaka <suthura@hotmail.com>
2020-02-27 18:04:00 +0530

    phone route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
 const mailRoute = require('./routes/resetpass');
+const phoneRoute = require('./routes/phone');
 
 var bodyParser = require('body-parser');
 
@@ -24,7 +25,7 @@ app.use('/uploads', express.static('uploads'));
 //route middleware
 app.use('/api/user', authRoute);
 app.use('/api/mail', mailRoute);
-
+app.use('/api/phone', phoneRoute);
 
 
 app.listen(process.env.PORT || 5000, () => console.log("Server up and running"));
