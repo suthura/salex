bd69b7e0219ef8d6b95c3c80dabdfdb2d4218b26
Suthura Sudharaka <suthura@hotmail.com>
2020-05-01 14:03:14 +0530

    update ref

--- routes/Admin/ref.js
+++ routes/Admin/ref.js
@@ -13,4 +13,28 @@ router.get('/allrefs', async(req, res) => {
 });
 
 
+router.post('/updateref', async(req, res) => {
+
+    var query = { _id: req.body.refID };
+
+    var newVal = {
+        $set: {
+            name: req.body.name,
+            address: req.body.address,
+            NIC: req.body.NIC,
+            email: req.body.email,
+            phone: req.body.phone,
+            imagesource: req.body.imagesource
+        }
+    }
+
+    await User.updateOne(query, newVal, function(err) {
+        if (err) {
+            res.send(err);
+        }
+        res.send({ "message": "success" })
+    });
+});
+
+
 module.exports = router;
