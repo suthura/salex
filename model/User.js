009150250fc6b6c0c7a732a66f26aca9804bfef7
Suthura Sudharaka <suthura@hotmail.com>
2020-05-21 02:37:58 +0530

    admin login

--- model/User.js
+++ model/User.js
@@ -36,6 +36,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         default: 'active'
+    },
+    password: {
+        type: String,
+        min: 6
     }
 });
 
--- routes/auth.js
+++ routes/auth.js
@@ -59,5 +59,62 @@ router.post('/checkphonenumber', async(req, res) => {
 });
 
 
+router.post('/registeragent', async(req, res) => {
+
+    console.log(req.body);
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(req.body.password, salt);
+
+    const user = new User({
+        name: req.body.name,
+        address: req.body.address,
+        NIC: req.body.NIC,
+        email: req.body.email,
+        phone: req.body.phone,
+        imagesource: req.body.imagesource,
+        usertype: "agent",
+        password: hashedpassword
+    });
+    try {
+        const savedUser = await user.save();
+
+        const token = jwt.sign({ _id: user._id, name: user.name }, process.env.TOKEN_SECRET);
+
+        res.header('auth-token', token).send({
+            loginstatus: 'olduser',
+            status: user.status,
+            token: token
+        });
+    } catch (err) {
+        res.send({
+            messege: err
+        });
+    }
+});
+
+router.post('/adminlogin', async(req, res) => {
+    const user = await User.findOne({
+        email: req.body.email,
+        usertype: "agent"
+    });
+    if (!user) return res.status(400).send({ message: 'Email Not Exist' });
+
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+
+    if (!validPass) return res.status(400).send({ message: 'Password Not Valid' });
+
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+
+
+    res.header('auth-token', token).send({
+        loginstatus: 'olduser',
+        status: user.status,
+        token: token,
+        usertype: user.usertype
+    });
+});
+
+
 
 module.exports = router;
